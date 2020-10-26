6159473101d67653bca46019deea496dce9c77f4
Julien Van Tongerloo <julien.vantongerloo@student.vinci.be>
2020-10-26 10:50:33 +0100

    refactor - return the int value

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -5,7 +5,6 @@ class BerlinClockKata
 {
     public function simple_minute(int $heure, int $minute): int
     {
-        if($minute === 1) return 1;
-        return 0;
+        return $minute;
     }
 }
